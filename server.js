083d7ef6c314d3563e0e1048ec5c694f6a463db3
GOKU1301 <debu1301@gmail.com>
2025-02-21 00:30:27 +0530

    enhanced chat ui

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const { saveMessage, getMessages } = require("./db"); // Import Firestore functions
+const { saveMessage, getMessages } = require("./server/db");
 
 const app = express();
 const server = http.createServer(app);
@@ -16,7 +16,7 @@ io.on("connection", (socket) => {
     socket.on("joinGroup", async ({ group, username }) => {
         socket.join(group);
         socket.group = group;
-        socket.username = username; // Store the username in the socket
+        socket.username = username;
 
         console.log(`User ${username} joined group: ${group}`);
         
@@ -25,15 +25,15 @@ io.on("connection", (socket) => {
         socket.emit("groupMessages", messages);
 
         // Notify other users in the group
-        socket.to(group).emit("userJoined", username); // Notify others
+        socket.to(group).emit("userJoined", username);
     });
 
     // Handle new message
     socket.on("sendMessage", async ({ group, message, sender }) => {
         if (socket.group) {
-            console.log(`Received message: '${message}' from ${sender} in group '${group}'`); // Log received message
-            await saveMessage(group, message, sender); // Save to Firestore
-            io.to(socket.group).emit("message", { group, message, sender }); // Broadcast
+            console.log(`Received message: '${message}' from ${sender} in group '${group}'`);
+            await saveMessage(group, message, sender);
+            io.to(socket.group).emit("message", { group, message, sender });
         }
     });
 
@@ -45,4 +45,4 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-});
+}); 
